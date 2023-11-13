2ae9157c3d0a2e607c411baacb730cb13a529196
hskim <rhgustmfrh@naver.com>
2023-11-13 11:24:47 +0900

    리파지토리, 서비스, 컨트롤러 추가 후 개인키 하드 코딩된 부분 수정 및 깃 이그노어 수정

--- src/main/java/com/backend/test/service/WeatherService.java
+++ src/main/java/com/backend/test/service/WeatherService.java
@@ -1,13 +1,16 @@
 package com.backend.test.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
+
     private static final String WEATHER_API_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
-    private static final String API_KEY = "{}"; // 발급받은 API 키
+    @Value("${go.data.apiKey}")
+    private String API_KEY; // 발급받은 API 키
 
     public String getWeather(String base_date, String base_time, String nx, String ny) {
         String url = WEATHER_API_URL + "?serviceKey=" + API_KEY
